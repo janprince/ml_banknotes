f67f53d425ae9b4ee814ae1c90c6e808705d7cf3
Prince Jan Kumi <janprince002@gmail.com>
2022-01-19 10:37:14 +0000

    SVC model

--- banknotes.py
+++ banknotes.py
@@ -6,7 +6,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 import csv
 
-model = Perceptron()
+model = SVC()
 
 # read data
 with open("dataset/banknotes.csv") as f:
@@ -16,8 +16,8 @@ with open("dataset/banknotes.csv") as f:
     data = []
     for row in reader:
         data.append({
-            "evidence" : [float(cell) for cell in row[:4]],
-            "label": "Authentic" if row[4] == 0 else "Counterfeit"
+            "evidence": [float(cell) for cell in row[:4]],
+            "label": "Authentic" if row[4] == "0" else "Counterfeit"
         })
 
 
@@ -28,3 +28,33 @@ testing = data[:holdout]
 training = data[holdout:]
 
 
+# Train model on training set
+X_training = [row["evidence"] for row in training]
+Y_training = [row["label"] for row in training]
+model.fit(X_training, Y_training)
+
+
+# Make predictions on the testing set
+X_testing = [row["evidence"] for row in testing]
+Y_testing = [row["label"] for row in testing]
+predictions = model.predict(X_testing)
+
+
+# Compute how well we performed
+correct = 0
+incorrect = 0
+total = 0
+
+for actual, predicted in zip(Y_testing, predictions):
+    total += 1
+    if actual == predicted:
+        correct += 1
+    else:
+        incorrect += 1
+
+
+# Print results
+print(f"Results for model: {type(model).__name__}")
+print(f"Correct: {correct}")
+print(f"Incorrect: {incorrect}")
+print(f"Accuracy: {100 * (correct/total):.2f}")
